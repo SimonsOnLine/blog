9df87f00910ecc4f00f0d1411f444da4540dfac9
liujiantao <liujiantao86@foxmail.com>
2020-04-12 21:42:55 +0800

    :green_apple:

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
-
+# npm install -g vuepress@next
 # 生成静态文件
-npm run build
+yarn build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -12,10 +14,11 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m ':checkered_flag:'
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<Ean-Liu>/<Ean-Liu>.github.io.git
+# git push -f https://${token}@${address} master:master
+git push -f https://${f685bfceaff3dbda2250553fdcdb58747211d775}@github.com/Ean-Liu/Ean-Liu.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
